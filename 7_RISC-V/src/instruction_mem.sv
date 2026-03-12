3de538559c97765e4cae19da418f091bc5cd07a7
unknown <sumin1201@ajou.ac.kr>
2026-03-12 11:39:13 +0900

    modify for full case simulation

--- 7_RISC-V/src/instruction_mem.sv
+++ 7_RISC-V/src/instruction_mem.sv
@@ -8,7 +8,7 @@ module instruction_mem(
     logic [31:0] rom[0:31];
 
     initial begin   // for simulation. sysnthesis x
-        //나머지 메모리 공간은 모두 x로 채워짐
+        // 정의하지 않은 메모리 공간은 모두 x로 채워짐
         // R-type
         rom[0] = 32'h004182b3;
         rom[1] = 32'h405201b3;
@@ -31,8 +31,35 @@ module instruction_mem(
         rom[17] = 32'h003116a3;
         rom[18] = 32'h00322723;
         // I-type
-        rom[19] = 32'h01002283; // LW x5, 16(x0)
-        rom[20] = 32'h00438413; // ADDI x8, x7, 4
+        //rom[19] = 32'h01002283; // LW x5, 16(x0)
+        //rom[20] = 32'h01101283; // LH x5, 16(x0)
+        //rom[21] = 32'h01200283; // LB x5, 16(x0)
+        //rom[22] = 32'h01004283; // LBU
+        //rom[23] = 32'h01005283; // LHU
+        // IL-type
+        rom[19] = 32'h00118293; // ADDI x8, x7, 4
+        rom[20] = 32'h0011a293; // SLTI
+        rom[21] = 32'h0011b293; // SLTIU
+        rom[22] = 32'h00aac293; // XORI
+        rom[23] = 32'h00ab6293; // ORI
+        rom[24] = 32'h00ab7293; // ANDI
+        rom[25] = 32'h00219293; // SLLI
+        rom[26] = 32'h0021d293; // SRLI
+        rom[27] = 32'h4021d293; // SRAI
+        // B-type
+        //rom[0] = 32'h402081b3;
+        //rom[1] = 32'h00108463; // BEQ true
+        //rom[3] = 32'h00209463; // BNE true
+        //rom[5] = 32'h0011c463; // BLT true
+        //rom[7] = 32'h0030d463; // BGE true
+        //rom[9] = 32'h0030e463; // BLTU true
+        //rom[11] = 32'h0011f463; // BGEU true
+        //rom[13] = 32'h00208463; // BEQ fail
+        //rom[14] = 32'h00109463; // BNE fail
+        //rom[15] = 32'h0010c463; // BLT fail
+        //rom[16] = 32'h0020d463; // BGE fail
+        //rom[17] = 32'h0011e463; // BLTU fail
+        //rom[18] = 32'h0030f463; // BGEU fail
     end
 
     assign instr_data = rom[instr_addr[31:2]]; // word adressing
