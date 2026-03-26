2927eb7890191742b529c005150258f69511ebe9
unknown <sumin1201@ajou.ac.kr>
2026-03-26 16:47:57 +0900

    create simulation code for APB_bus(BRAM, GPO, GPI).

--- 7_RISC-V/src/instruction_mem.sv
+++ 7_RISC-V/src/instruction_mem.sv
@@ -8,7 +8,7 @@ module instruction_mem(
     logic [31:0] rom[0:127];
 
     initial begin   // for simulation. sysnthesis x
-        $readmemh("APB_GPO.mem", rom);
+        $readmemh("APB_BRAM_GPO_GPI.mem", rom);
 
         // 정의하지 않은 메모리 공간은 모두 x로 채워짐
         // R-type
