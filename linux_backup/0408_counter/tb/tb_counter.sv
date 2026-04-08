40bdd7c9a21785d82fe288fe5b3635a697c1c459
suminY9 <sumin.y9922@gmail.com>
2026-04-08 14:26:12 +0900

    add scoreboard

--- linux_backup/0408_counter/tb/tb_counter.sv
+++ linux_backup/0408_counter/tb/tb_counter.sv
@@ -5,6 +5,19 @@ interface counter_if(input logic clk);
     logic       rst_n;
     logic       enable;
     logic [3:0] count;
+
+    clocking drv_cb @(posedge clk);
+        default input #1step output #0;
+        output rst_n;
+        output enable;
+    endclocking
+
+    clocking mon_cb @(posedge clk);
+        default input #1step;
+        input rst_n;
+        input enable;
+        input count;
+    endclocking
 endinterface
 
 
@@ -12,7 +25,7 @@ endinterface
 class counter_seq_item extends uvm_sequence_item;
     rand bit rst_n;
     rand bit enable;
-    rand bit cycles;
+    rand int cycles;
     logic [3:0] count;
 
     constraint c_cycles {cycles inside {[1 : 20]};}
@@ -128,10 +141,12 @@ class counter_driver extends uvm_driver #(counter_seq_item);
     endfunction
 
     virtual task drive_item(counter_seq_item item);
-        c_if.rst_n  <= item.rst_n;
-        c_if.enable <= item.enable;
-        repeat(item.cycles) @(posedge c_if.clk);
+        c_if.drv_cb.rst_n  <= item.rst_n;
+        c_if.drv_cb.enable <= item.enable;
+        //repeat(item.cycles) @(posedge c_if.clk);
+        repeat(item.cycles) @(c_if.drv_cb);
         `uvm_info(get_type_name(), $sformatf("drive_cycles: %0d", item.cycles), UVM_HIGH);
+        `uvm_info(get_type_name(), item.convert2string(), UVM_MEDIUM);
     endtask
 
     virtual task run_phase(uvm_phase phase);
@@ -165,11 +180,12 @@ class counter_monitor extends uvm_monitor;
         forever begin
             counter_seq_item item = counter_seq_item::type_id::create("item");
             // ...... interface 신호 수집
-            @(c_if.clk);
+            //@(c_if.clk);
+            @(c_if.mon_cb);
             #1;
-            item.rst_n = c_if.rst_n;
-            item.enable = c_if.enable;
-            item.count = c_if.count;
+            item.rst_n  = c_if.mon_cb.rst_n;
+            item.enable = c_if.mon_cb.enable;
+            item.count  = c_if.mon_cb.count;
             ap.write(item);
             `uvm_info(get_type_name(), item.convert2string(), UVM_MEDIUM);
         end
@@ -182,18 +198,65 @@ class counter_scoreboard extends uvm_scoreboard;
     // analysis implementation 선언, write 함수를 구현하는 부분
     uvm_analysis_imp #(counter_seq_item, counter_scoreboard) ap_imp;
 
+    logic [3:0] expected;
+    int error_count;
+    int match_count;
+    bit first_transaction;
+
     function new(string name, uvm_component parent);
         super.new(name, parent);
+        ap_imp = new("ap_imp", this);
+        error_count = 0;
+        match_count = 0;
+        first_transaction = 1;
+        expected = 0;
     endfunction
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        ap_imp = new("ap_imp", this);
     endfunction
 
     virtual function void write(counter_seq_item item);
         `uvm_info(get_type_name(), $sformatf("Received: %s", item.convert2string()), UVM_MEDIUM);
         // 검증 로직
+
+        if(first_transaction) begin
+            `uvm_info(get_type_name(), $sformatf("Initial state: %s", item.convert2string()), UVM_MEDIUM)
+            first_transaction = 0;
+            return;
+        end
+
+        // 2. 예측 vs 실제 비교 판단
+        if(expected !== item.count) begin
+            `uvm_error(get_type_name(), $sformatf("MISMATCH! expected=%0d, actual=%0d (rst_n=%0b, enable=%0b)", expected, item.count, item.rst_n, item.enable))
+            error_count++;
+        end else begin
+           `uvm_info(get_type_name(), $sformatf("MATCH!: expected=%0d, count=%0d (rst_n=%0b, enable=%0b)", expected, item.count, item.rst_n, item.enable), UVM_LOW)
+           match_count++; 
+        end
+
+        // 1. reference model 게산 예측
+        if(!item.rst_n) begin
+            expected = 0;
+        end
+        else if(item.enable) begin
+            expected = expected + 1;
+        end
+    endfunction
+
+    virtual function void report_phase (uvm_phase phase);
+        super.report_phase(phase);
+        `uvm_info(get_type_name(), " ===== Scoreboad Summary =====", UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("  Total transactions: %0d", match_count + error_count), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("  Matches: %0d", match_count), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("  Error: %0d", error_count), UVM_LOW)
+
+        if(error_count > 0) begin
+            `uvm_error(get_type_name(), $sformatf("TEST FAILED: %0d mismataches detected!", error_count))
+        end
+        else begin
+            `uvm_info(get_type_name(), $sformatf("TEST PASSED: %0d matches detected!", match_count), UVM_LOW)
+        end
     endfunction
 endclass
 
@@ -235,7 +298,6 @@ class counter_environment extends uvm_env;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
-        `uvm_info(get_type_name(), "new 실행", UVM_DEBUG);
     endfunction
 
     /***** phase *****/
