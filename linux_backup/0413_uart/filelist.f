146d8d8739bfe649120a9a46734840227170468e
suminY9 <sumin.y9922@gmail.com>
2026-04-13 16:24:00 +0900

    add uart_rx & complete testbench

--- linux_backup/0413_uart/filelist.f
+++ linux_backup/0413_uart/filelist.f
@@ -1,4 +1,6 @@
 ./rtl/baud_rate_gen.sv
 ./rtl/uart_tx.sv
+./rtl/uart_rx.sv
 ./rtl/uart.sv
-./tb/tb_uart.sv
+./rtl/sync_2ff.sv
+./tb/tb_uart_test.sv
---
+++ linux_backup/0413_uart/rtl/sync_2ff.sv
@@ -0,0 +1,21 @@
+module sync_ff (
+    input  logic clk,
+    input  logic reset,
+    input  logic async_in,
+    output logic sync_out
+);
+
+    logic ff1, ff2;
+
+    assign sync_out = ff2;
+
+    always_ff @(posedge clk or posedge reset) begin
+        if(reset) begin
+            ff1 <= 1'b1;
+        end else begin
+            ff1 <= async_in;
+            ff2 <= ff1;
+        end
+    end
+
+endmodule
